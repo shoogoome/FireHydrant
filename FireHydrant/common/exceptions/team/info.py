5cd8d73881a8710d64fb598324c01019c9cec4bd
121 <shoogoome@sina.com>
2019-07-20 20:45:47 +0800

    add添加队伍异常信息

--- FireHydrant/common/exceptions/team/info.py
+++ FireHydrant/common/exceptions/team/info.py
@@ -8,3 +8,11 @@ class TeamInfoExcept(FireHydrantExceptBase):
     @classmethod
     def already_in_team(cls):
         return cls("已加入队伍不得创建队伍")
+
+    @classmethod
+    def team_is_not_exists(cls):
+        return cls("队伍不存在")
+
+    @classmethod
+    def nickname_is_exists(cls):
+        return cls("队伍名已存在")
