ac43e0a2f6b2f5b58614bdc42892ad9dc4286965
121 <shoogoome@sina.com>
2019-07-20 22:02:17 +0800

    change添加参数str长度限制

--- FireHydrant/common/exceptions/common/paramsExcept.py
+++ FireHydrant/common/exceptions/common/paramsExcept.py
@@ -33,5 +33,13 @@ class ParamsExcept(FireHydrantExceptBase):
     def model_diff_error(cls):
         return cls("不得在diff模式下运行")
 
+    @classmethod
+    def exceed_max_length(cls, desc, length):
+        return cls("{}不得超过最大限制长度: {}字符".format(desc, length))
+
+    @classmethod
+    def is_not_allow_less(cls, desc, length):
+        return cls("{}不得少于{}个字符".format(desc, length))
+
 
 
