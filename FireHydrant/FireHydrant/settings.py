d586c6f0499de643bdba2c4750a8021242b3e567
121 <shoogoome@sina.com>
2019-07-22 14:44:05 +0800

    add添加跨域设置

--- FireHydrant/FireHydrant/settings.py
+++ FireHydrant/FireHydrant/settings.py
@@ -41,6 +41,34 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+
+
 # ##################
 #      Celery
 # ##################
