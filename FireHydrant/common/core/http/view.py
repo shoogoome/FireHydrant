2176e43a30fd6a52ec3dfa66299d410b75ce41b0
121 <shoogoome@sina.com>
2019-07-22 20:49:16 +0800

    change

--- FireHydrant/common/core/http/view.py
+++ FireHydrant/common/core/http/view.py
@@ -113,6 +113,12 @@ class FireHydrantViewBase(View):
         if len(self.__setcookies) > 0:
             for c in self.__setcookies:
                 response.set_cookie(**c)
+        # 允许跨域
+        # response["Access-Control-Allow-Origin"] = "http://localhost:8000"
+        # response['Access-Control-Allow-Credentials'] = 'true'
+        # response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+        # response["Access-Control-Max-Age"] = "1000"
+        # response["Access-Control-Allow-Headers"] = "*"
 
         return response
 
