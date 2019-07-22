c9ef5aef4a095505f83bbcbca37a88ac472b3411
121 <shoogoome@sina.com>
2019-07-22 17:15:40 +0800

    delete删除跨域

--- FireHydrant/common/core/http/view.py
+++ FireHydrant/common/core/http/view.py
@@ -114,10 +114,10 @@ class FireHydrantViewBase(View):
             for c in self.__setcookies:
                 response.set_cookie(**c)
         # 允许跨域
-        response["Access-Control-Allow-Origin"] = "http://localhost:8080, http://localhost:8000"
-        response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
-        response["Access-Control-Max-Age"] = "1000"
-        response["Access-Control-Allow-Headers"] = "*"
+        # response["Access-Control-Allow-Origin"] = "http://localhost:8080, http://localhost:8000"
+        # response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+        # response["Access-Control-Max-Age"] = "1000"
+        # response["Access-Control-Allow-Headers"] = "*"
 
         return response
 
