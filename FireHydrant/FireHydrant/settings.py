6ba4591bfa58170cd4b6547d1f4972d3870d48f2
121 <shoogoome@sina.com>
2019-07-22 16:29:59 +0800

    change

--- FireHydrant/FireHydrant/settings.py
+++ FireHydrant/FireHydrant/settings.py
@@ -199,8 +199,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    '*',
-    'localhost:8080',
+    'http://localhost:8080',
 )
 CORS_ALLOW_METHODS = (
     'DELETE',
