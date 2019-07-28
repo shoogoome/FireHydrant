0ca917d9f456ded4fe75b72fba24cb3b208569e8
121 <shoogoome@sina.com>
2019-07-28 22:36:53 +0800

    update 添加视图__all__

---
+++ FireHydrant/server/task/views/__init__.py
@@ -0,0 +1,7 @@
+from .classification.info import TaskClassificationInfoView
+from .classification.list import TaskClassificationListView
+
+__all__ = [
+    # 任务分类
+    'TaskClassificationInfoView', 'TaskClassificationListView',
+]
