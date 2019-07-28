2fd4cb09a5643c5c6dc01f1cae761f6b7fd7a6d6
121 <shoogoome@sina.com>
2019-07-28 23:04:51 +0800

    update 添加分类渲染

--- FireHydrant/server/task/admin.py
+++ FireHydrant/server/task/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Task, TaskReport
+from .models import Task, TaskReport, TaskClassification
 # Register your models here.
 
 admin.site.register(Task)
 admin.site.register(TaskReport)
+admin.site.register(TaskClassification)
