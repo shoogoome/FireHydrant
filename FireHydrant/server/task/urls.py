843558625ecbbddeb06c82d868c646c8b98a60a2
121 <shoogoome@sina.com>
2019-07-28 22:37:15 +0800

    add 添加分类业务代码

---
+++ FireHydrant/server/task/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .views import *
+
+
+classification_urlpatterns = [
+    path('', TaskClassificationInfoView.as_view(method=['POST'])),
+    path('/<int:cid>', TaskClassificationInfoView.as_view(method=['GET', 'PUT', 'DELETE'])),
+    path('/list', TaskClassificationListView.as_view(method=['GET'])),
+]
+
+urlpatterns = [
+    path('/classifications', include(classification_urlpatterns)),
+]
--- FireHydrant/server/task/views/classification/info.py
+++ FireHydrant/server/task/views/classification/info.py
@@ -6,12 +6,11 @@ from django.db import transaction
 from common.constants.length_limitation import *
 from common.core.auth.check_login import check_login
 from common.core.http.view import FireHydrantView
-from common.enum.team.role import TeamRoleEnum
-from common.exceptions.team.info import TeamInfoExcept
+from common.exceptions.task.classification import TaskClassificationExcept
 from common.utils.helper.params import ParamsParser
 from common.utils.helper.result import SuccessResult
+from ...logics.classification import TaskClassificationLogic
 from ...models import TaskClassification
-from common.exceptions.task.classification import TaskClassificationExcept
 
 
 class TaskClassificationInfoView(FireHydrantView):
@@ -32,8 +31,9 @@ class TaskClassificationInfoView(FireHydrantView):
 
         with transaction.atomic():
             classification = TaskClassification.objects.create(
-                name=params.str('name', desc='分类名称'),
-                description=params.str('description', desc='简介', default='', require=False),
+                name=params.str('name', desc='分类名称', max_length=MAX_CLASSIFICATION_LENGTH),
+                description=params.str('description', desc='简介', default='', require=False,
+                                       max_length=MAX_DESCRIPETION_LENGTH),
             )
 
         if params.int('parent'):
@@ -42,25 +42,44 @@ class TaskClassificationInfoView(FireHydrantView):
 
         return SuccessResult(id=classification.id)
 
-    def get(self, request):
+    def get(self, request, cid):
         """
         获取任务分类信息
         :param request:
+        :param cid:
         :return:
         """
-        ...
+        logic = TaskClassificationLogic(self.auth, cid)
+        return SuccessResult(logic.get_classification_info())
 
-    def put(self, request):
+    @check_login
+    def put(self, request, cid):
         """
         修改分类信息
         :param request:
+        :param cid:
         :return:
         """
-        ...
+        params = ParamsParser(request.JSON)
+        logic = TaskClassificationLogic(self.auth, cid)
 
-    def delete(self, request):
+        classification = logic.classification
+        with params.diff(classification):
+            classification.name = params.str('name', desc='名称', max_length=MAX_CLASSIFICATION_LENGTH)
+            classification.description = params.str('description', desc='简介', max_length=MAX_DESCRIPETION_LENGTH)
+        classification.save()
+
+        return SuccessResult(id=cid)
+
+    @check_login
+    def delete(self, request, cid):
         """
         删除分类信息
         :param request:
+        :param cid:
         :return:
         """
+        logic = TaskClassificationLogic(self.auth, cid)
+        logic.classification.delete()
+
+        return SuccessResult(id=cid)
