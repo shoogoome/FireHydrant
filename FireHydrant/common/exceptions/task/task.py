b83adb0c11284ba0b222df88585022359e866b81
121 <shoogoome@sina.com>
2019-08-01 15:57:08 +0800

    add 添加任务异常

---
+++ FireHydrant/common/exceptions/task/task.py
@@ -0,0 +1,14 @@
+
+from common.exceptions.base import FireHydrantExceptBase
+
+class TaskInfoExcept(FireHydrantExceptBase):
+
+    MAJOR_HTTP_CODE = 553.2
+
+    @classmethod
+    def task_is_not_exists(cls):
+        return cls("任务不存在")
+
+    @classmethod
+    def task_type_is_not_exists(cls):
+        return cls("分类不存在")
