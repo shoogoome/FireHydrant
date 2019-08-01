f898132b3be28dc6733d422b51cbb3e7020ad453
121 <shoogoome@sina.com>
2019-08-01 18:12:51 +0800

    update

--- FireHydrant/common/exceptions/task/task.py
+++ FireHydrant/common/exceptions/task/task.py
@@ -12,3 +12,9 @@ class TaskInfoExcept(FireHydrantExceptBase):
     @classmethod
     def task_type_is_not_exists(cls):
         return cls("分类不存在")
+
+    @classmethod
+    def is_not_in_release(cls):
+        return cls("任务不在发布期不允许修改基础信息")
+
+
