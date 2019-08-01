d3bdfd7ef4f22b581474d6c76ecdeb63d3d93e62
121 <shoogoome@sina.com>
2019-08-02 00:03:07 +0800

    update

--- FireHydrant/common/exceptions/task/classification.py
+++ FireHydrant/common/exceptions/task/classification.py
@@ -8,3 +8,7 @@ class TaskClassificationExcept(FireHydrantExceptBase):
     @classmethod
     def classification_is_not_exists(cls):
         return cls("分类不存在")
+
+    @classmethod
+    def parent_is_not_exists(cls):
+        return cls("父节点不存在")
