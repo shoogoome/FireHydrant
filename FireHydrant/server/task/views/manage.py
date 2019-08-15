483022e1110dcd7e1cf767bcb0a152fe4569832b
121 <shoogoome@sina.com>
2019-08-15 20:09:54 +0800

    add 添加任务业务带啊吗

--- FireHydrant/server/task/views/manage.py
+++ FireHydrant/server/task/views/manage.py
@@ -13,6 +13,8 @@ from common.utils.helper.params import ParamsParser
 from common.utils.helper.result import SuccessResult
 from ..logics.task import TaskLogic
 from ..models import Task, TaskClassification, TaskApply
+from common.enum.task.stage import TaskStageEnum
+from server.account.models import AccountExhibition
 
 
 class TaskManageView(FireHydrantView):
@@ -35,4 +37,30 @@ class TaskManageView(FireHydrantView):
         :param tid:
         :return:
         """
-        ...
+        logic = TaskLogic(self.auth, tid)
+        params = ParamsParser(request.JSON)
+
+        if logic.task.stage != int(TaskStageEnum.RELEASE):
+            raise TaskInfoExcept.no_permission()
+
+        if params.has('exhibition'):
+            # 成就选择异常 （一般是恶意请求，所以直接报无权限）
+            exhibitions = AccountExhibition.objects.filter(account=self.auth.get_account()).values('id')
+            ex_ids = params.list('exhibition')
+            if not set(ex_ids).issubset(exhibitions):
+                raise TaskInfoExcept.no_permission()
+
+        with transaction.atomic():
+            apply = TaskApply.objects.create(
+                task=logic.task,
+                author=self.auth.get_account(),
+                content=params.str('content', desc='正文'),
+            )
+        if params.has('exhibition'):
+            apply.exhibition.set(ex_ids)
+            apply.save()
+
+        return SuccessResult(id=apply.id)
+
+
+
