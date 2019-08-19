3bf1e79642bf86b8a4d9cb5f903649fc6d6d2e33
121 <shoogoome@sina.com>
2019-08-19 12:26:20 +0800

    add 添加资源逻辑

---
+++ FireHydrant/server/resources/logic/info.py
@@ -0,0 +1,36 @@
+from ..models import ResourcesMeta
+from common.exceptions.resources.info import ResourceInfoExcept
+from .client import LiumaClient
+
+class ResourceLogic(object):
+
+    def __init__(self, auth, mid):
+        """
+        INIT
+        :param auth:
+        :param mid:
+        """
+        self.auth = auth
+        self.client = LiumaClient()
+
+        if isinstance(mid, ResourcesMeta):
+            self.meta = mid
+        else:
+            self.meta = self.get_meta_model(mid)
+
+    def get_meta_model(self, mid):
+        """
+        获取资源元数据信息
+        :param mid:
+        :return:
+        """
+        if mid is None or mid == '':
+            return
+
+        meta = ResourcesMeta.objects.get_once(pk=mid)
+        if meta is None:
+            raise ResourceInfoExcept.meta_is_not_exists()
+
+        return meta
+
+
