d1f4efb18fc830202c3a19387f928f9f6debb8f3
121 <shoogoome@sina.com>
2019-08-19 12:26:10 +0800

    change 资源路由加上id

--- FireHydrant/server/resources/urls.py
+++ FireHydrant/server/resources/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
 
-    path('/upload', ResourcesInfoView.as_view(method=['GET'], is_upload=True)),
-    path('/download', ResourcesInfoView.as_view(method=['GET'])),
+    path('/upload/<int:mid>', ResourcesInfoView.as_view(method=['GET'], is_upload=True)),
+    path('/download/<int:mid>', ResourcesInfoView.as_view(method=['GET'])),
 ]
--- FireHydrant/server/resources/views/info.py
+++ FireHydrant/server/resources/views/info.py
@@ -4,21 +4,23 @@
 from common.core.http.view import FireHydrantView
 from common.utils.helper.params import ParamsParser
 from common.utils.helper.result import SuccessResult
-from ..logic.client import LiumaClient
+from common.core.auth.check_login import check_login
+from ..logic.info import ResourceLogic
 
 
 class ResourcesInfoView(FireHydrantView):
     is_upload = False
 
-    def get(self, request):
+    @check_login
+    def get(self, request, mid):
         """
         获取令牌
         :param request:
+        :param mid:
         :return:
         """
-        params = ParamsParser(request.GET)
+        logic = ResourceLogic(self.auth, mid)
 
-        client = LiumaClient()
         if self.is_upload:
-            return SuccessResult(token=client.get_upload_token(params.str('hash', desc='文件hash')))
-        return SuccessResult(token=client.get_download_token(params.str('hash', desc='文件hash')))
+            return SuccessResult(token=logic.client.get_upload_token(logic.meta.hash))
+        return SuccessResult(token=logic.client.get_download_token(logic.meta.hash))
