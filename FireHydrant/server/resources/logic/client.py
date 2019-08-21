1e2a2edfa771de4d9baa1afe85e55332d1211ec9
121 <shoogoome@sina.com>
2019-08-21 13:41:54 +0800

    change 修改资源逻辑

--- FireHydrant/server/resources/logic/client.py
+++ FireHydrant/server/resources/logic/client.py
@@ -11,8 +11,10 @@ class LiumaClient(object):
         流马文件服务器客户端
         """
         self.base_url = 'http://liuma.shoogoome.com'
-        self.download_url = '/download/token'
-        self.upload_url = '/upload/token'
+        self.download_token_url = '/download/token'
+        self.upload_token_url = '/upload/token'
+        self.upload_finish_url = '/upload/finish'
+        self.file_info_url = '/file/info'
         self.headers = {
             'systemToken': LIUMA_SYSTEM_TOKEN
         }
@@ -23,7 +25,7 @@ class LiumaClient(object):
         :param fhash 文件hash值
         :return:
         """
-        return self.get_token('{}{}'.format(self.base_url, self.download_url), fhash)
+        return self.get_token('{}{}'.format(self.base_url, self.download_token_url), fhash)
 
     def get_upload_token(self, fhash):
         """
@@ -31,8 +33,31 @@ class LiumaClient(object):
         :param fhash:
         :return:
         """
-        return self.get_token('{}{}'.format(self.base_url, self.upload_url), fhash)
+        return self.get_token('{}{}'.format(self.base_url, self.upload_token_url), fhash)
 
+    def upload_finish(self, token, name):
+        """
+        完成上传
+        :param token:
+        :param name:
+        :return:
+        """
+        self.headers['token'] = token
+        # 查看是否为单文件上传
+        response = requests.get("{}{}".format(self.base_url, self.file_info_url), headers=self.headers)
+        try:
+            data = response.json()
+            if data.get('data', None) is not None:
+                return True
+        except:
+            pass
+        # 分片合并
+        response = requests.get('{}{}?fire_name={}'.format(
+            self.base_url, self.upload_finish_url, name), headers=self.headers)
+
+        if response.status_code != 200:
+            return False
+        return True
 
     def get_token(self, url, fhash):
         """
--- FireHydrant/server/resources/logic/info.py
+++ FireHydrant/server/resources/logic/info.py
@@ -1,6 +1,10 @@
 from ..models import ResourcesMeta
 from common.exceptions.resources.info import ResourceInfoExcept
+from common.utils.hash.signatures import cookie_signature
 from .client import LiumaClient
+import time
+import json
+import base64
 
 class ResourceLogic(object):
 
@@ -42,12 +46,63 @@ class ResourceLogic(object):
             return ""
         return self.client.get_download_token(self.meta.hash)
 
-    def get_upload_token(self):
+    @staticmethod
+    def get_upload_token(hash):
         """
         获取上传token
+        :param hash:
+        :return:
+        """
+        client = LiumaClient()
+        return client.get_upload_token(hash)
+
+    def upload_finish(self, token, name, expire=3600):
+        """
+        完成上传并生成token token过期时间一小时
+        :param token:
+        :param name:
+        :param expire:
         :return:
         """
         if self.meta is None:
-            return ""
-        return self.client.get_upload_token(self.meta.hash)
+            return False, ""
 
+        ok = self.client.upload_finish(token, name)
+        # 成功上传则创建token
+        if ok:
+            payload = {
+                "expire_at": int(time.time() + expire),
+                "meta": self.meta.id
+            }
+            payload_str = json.dumps(payload)
+            token = cookie_signature(payload_str)
+            payload_encode = base64.b64encode(payload_str.encode("utf-8")).decode()
+
+            return True, '{}.{}'.format(payload_encode, token)
+        # 否则删除当前元数据记录
+        else:
+            self.meta.delete()
+        return False, ""
+
+    @staticmethod
+    def decode_token(token):
+        """
+        解码token
+        :param token:
+        :return:
+        """
+        token_list = token.split('.')
+        # 格式错误
+        if len(token_list) != 2:
+            return None
+
+        payload_str = base64.b64decode(token_list[0]).decode()
+        # 签名验证
+        if cookie_signature(payload_str) != token_list[1]:
+            return None
+
+        payload = json.loads(payload_str)
+        if int(time.time()) >= payload.get('expire_at', -1):
+            return None
+        meta_id = payload.get('meta', -1)
+        return meta_id
