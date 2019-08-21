e8ac1d91951d16bcf873b0eca1e3cbcda201ac22
121 <shoogoome@sina.com>
2019-08-21 13:42:19 +0800

    update 更新资源路由

--- FireHydrant/server/resources/urls.py
+++ FireHydrant/server/resources/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-
-    # path('/upload/<int:mid>', ResourcesInfoView.as_view(method=['GET'], is_upload=True)),
-    # path('/download/<int:mid>', ResourcesInfoView.as_view(method=['GET'])),
+    # 获取上传路由
+    path('/upload/token', ResourcesInfoView.as_view(method=['GET'])),
+    # 完成上传
+    path('/upload/finish', ResourcesInfoView.as_view(method=['POST'])),
 ]
