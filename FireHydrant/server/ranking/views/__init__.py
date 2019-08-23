c8cba11f048130810b4cc971560ea07cd1ca2b1e
121 <shoogoome@sina.com>
2019-08-23 17:22:50 +0800

    add 添加排行榜业务代码

---
+++ FireHydrant/server/ranking/views/__init__.py
@@ -0,0 +1,5 @@
+from .task import RankingTaskView
+
+__all__ = [
+    'RankingTaskView'
+]
---
+++ FireHydrant/server/ranking/views/task.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# coding: utf-8
+
+from common.core.http.view import FireHydrantView
+from common.utils.helper.params import ParamsParser
+from common.utils.helper.result import SuccessResult
+from ..logics.info import RankingLogic
+
+class RankingTaskView(FireHydrantView):
+
+    def get(self, request):
+        """
+        任务排行榜
+        :param request:
+        :return:
+        """
+        params = ParamsParser(request.GET)
+        ranking_type = params.int('ranking_type', desc='榜单类型', require=False, default=0)
+        task_type = params.int('task_type', desc='任务类型', require=False, default=0)
+
+        return SuccessResult(RankingLogic.get_task_ranking(task_type=task_type, ranking_type=ranking_type))
