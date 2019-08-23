684eec53bb560c6c04a4016d18cf8d058f8b4bd5
121 <shoogoome@sina.com>
2019-08-23 10:52:05 +0800

    add 添加排行榜基础设置

--- FireHydrant/server/ranking/models.py
+++ FireHydrant/server/ranking/models.py
@@ -2,3 +2,44 @@ from django.db import models
 
 # Create your models here.
 
+"""
+任务榜:
+
+分日榜，月榜，季榜，年榜, 总榜
+
+分任务数
+
+分输出方和输入方
+
+{
+    "people_id": {
+        "name": "xxx",
+        "num": 123,
+    },
+    "people_id": {
+        "name": "xxx",
+        "num": 12,
+    }
+}
+
+财富榜:
+
+分日榜，月榜，季榜，年榜，总榜
+
+分金额进出数量
+
+分输出方和输入方
+
+
+{
+    "people_id": {
+        "name": "xxx",
+        "money": 123,
+    },
+    "people_id": {
+        "name": "xxx",
+        "money": 12,
+    }
+}
+
+"""
