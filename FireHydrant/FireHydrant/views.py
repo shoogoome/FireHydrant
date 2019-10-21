db56967670017110de2ec3cd83817a5bb5bf6972
121 <shoogoome@sina.com>
2019-10-21 16:24:49 +0800

    change 修改首页

--- FireHydrant/FireHydrant/views.py
+++ FireHydrant/FireHydrant/views.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 # coding: utf-8
 
-from django.shortcuts import redirect
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return redirect("https://github.com/shoogoome/FireHydrant")
-
-
-
-
-
-
-
-
+    return HttpResponse("<h1>粤ICP备19050376号</h1>")
 
 
 
