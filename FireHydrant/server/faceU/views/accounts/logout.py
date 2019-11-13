7f22ea0e62b034449f0c9e643bb5ded7f926d236
121 <shoogoome@sina.com>
2019-11-13 16:04:54 +0800

    add 登出接口

---
+++ FireHydrant/server/faceU/views/accounts/logout.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# coding: utf-8
+
+
+import json
+
+import requests
+
+from common.core.http.facec import FireHydrantFacecView
+from common.enum.account.role import AccountRoleEnum
+from common.exceptions.account.info import AccountInfoExcept
+from common.utils.helper.params import ParamsParser
+from common.utils.helper.result import SuccessResult
+from server.faceU.models import FaceUAccount
+
+
+class FaceUAccountLogout(FireHydrantFacecView):
+
+    def get(self, request):
+        """
+        登出
+        :param request:
+        :return:
+        """
+        self.auth.set_account(None)
+        self.auth.set_session()
+        self.auth.set_cookie()
+
+        return SuccessResult(status="ok")
